3182c90c04e2f33198b834d862c05367182460f2
Swapnil Kher <49282668+swapnil-kher@users.noreply.github.com>
2019-05-06 20:29:35 +0530

    Create PrintAllNaturalNumbersInReverseOrder.c

---
+++ PrintAllNaturalNumbersInReverseOrder.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    while(n>=1)
+    {
+        printf("%d\n", n);
+        n--;
+    }
+
+    return 0;
+}
